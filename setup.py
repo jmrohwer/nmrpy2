084cc8481d2895f6ab6b86bf3eedb13cd3bb73ec
jeicher <johanneicher@gmail.com>
2014-06-23 09:55:27 +0200

    packages for distribution

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='NMRPy',
+      version='1.0',
+      py_modules=['nmrpy'],
+      description='NMR processing utilities for arrayed spectra.',
+      author='Johann Eicher', 
+      author_email='johanneicher@gmail.com',
+      url='https://github.com/jeicher/NMRPy',
+      )
